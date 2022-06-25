6643ed5c5300d5f3ae50e68966ecce4f95e331f1
Gurabas <cuper8555@gmail.com>
2022-06-25 19:05:42 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let string = Math.abs(n).toString();
+    let array = string.split('');
+    let number = array.reverse().join('');
+    return number;
+
 }
